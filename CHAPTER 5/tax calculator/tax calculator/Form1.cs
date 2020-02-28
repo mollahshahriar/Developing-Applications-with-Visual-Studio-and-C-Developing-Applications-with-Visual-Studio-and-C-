953793290e0511b084a0774bcd20c685b11d35da
Shahriar <mollahshahriar@outlook.com>
2020-02-27 19:26:48 -0500

    jiiii

---
+++ CHAPTER 5/tax calculator/tax calculator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tax_calculator
+{
+    public partial class E : Form
+    {
+        public E()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form myform = new Form();
+            myform.ShowDialog();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
