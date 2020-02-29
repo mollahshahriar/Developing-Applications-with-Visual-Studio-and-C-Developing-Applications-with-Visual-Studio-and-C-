3566055d30b8e43bd499a9f8dbb1be4ad2aa7e16
Shahriar <mollahshahriar@outlook.com>
2020-02-28 19:11:01 -0500

    ffffff

---
+++ CHAPTER 9/Ch09Ex03/Ch09Ex03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ch09Ex03
+{
+    class Myclass
+    {
+        public int val;
+    }
+    struct myStruct
+    {
+        public int val;
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass objectA = new Myclass();
+            Myclass objectB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+
+            Console.WriteLine("objectA.val = {0}", objectA.val);
+            Console.WriteLine("objectA.val = {0}", objectB.val);
+            Console.WriteLine("structA.val = {0}", structA.val);
+            Console.WriteLine("structB.val = {0}", structB.val);
+            Console.ReadKey();                          
+        }
+    }
+}
